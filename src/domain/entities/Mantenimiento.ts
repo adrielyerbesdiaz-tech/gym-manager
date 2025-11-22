2ec07a8f88fc3c6ff4903acc7c00804eda47b951
Tony-Pereira05 <A13000882@alumnos.uady.mx>
2025-11-21 23:55:08 -0600

    Servicios agregados

--- src/domain/entities/Mantenimiento.ts
+++ src/domain/entities/Mantenimiento.ts
@@ -1,11 +1,53 @@
-export class Mantenimiento{
-    public readonly mantenimientoId: number;
-    public readonly tipoMantenimiento: string;
-    public readonly fechaMantenimiento: Date;
+export class Mantenimiento {
+    private readonly mantenimientoId: number;
+    private readonly equipoID: number;
+    private descripcion: string;
+    private readonly fechaInicio: Date;
+    private fechaFin: Date | null;
+    private costo: number;
 
-    constructor(tipoMantenimiento: string){
+    constructor(equipoID: number, descripcion: string) {
         this.mantenimientoId = 0;
-        this.tipoMantenimiento = tipoMantenimiento;
-        this.fechaMantenimiento = new Date();
+        this.equipoID = equipoID;
+        this.descripcion = descripcion;
+        this.fechaInicio = new Date();
+        this.fechaFin = null;
+        this.costo = 0;
+    }
+
+    public getMantenimientoId(): number {
+        return this.mantenimientoId;
+    }
+
+    public getEquipoID(): number {
+        return this.equipoID;
+    }
+
+    public getDescripcion(): string {
+        return this.descripcion;
+    }
+
+    public getFechaInicio(): Date {
+        return this.fechaInicio;
+    }
+
+    public getFechaFin(): Date | null {
+        return this.fechaFin;
+    }
+
+    public getCosto(): number {
+        return this.costo;
+    }
+
+    public setDescripcion(descripcion: string): void {
+        this.descripcion = descripcion;
+    }
+
+    public setFechaFin(fechaFin: Date): void {
+        this.fechaFin = fechaFin;
+    }
+
+    public setCosto(costo: number): void {
+        this.costo = costo;
     }
 }
--- src/domain/entities/Usuario.ts
+++ src/domain/entities/Usuario.ts
@@ -1,12 +1,14 @@
-export class Usuario{
+export class usuario {
     private readonly usuarioId: number;
     private nombreUsuario: string;
     private contrasenaHash: string;
+    private email: string;
 
-    constructor(nombreUsuario: string, contrasenaHash: string){
+    constructor(nombreUsuario: string, contrasenaHash: string, email: string) {
         this.usuarioId = 0;
         this.nombreUsuario = nombreUsuario;
         this.contrasenaHash = contrasenaHash;
+        this.email = email;
     }
 
     public getUsuarioId(): number {
@@ -21,11 +23,19 @@ export class Usuario{
         return this.contrasenaHash;
     }
 
+    public getEmail(): string {
+        return this.email;
+    }
+
     public setNombreUsuario(nombreUsuario: string): void {
         this.nombreUsuario = nombreUsuario;
     }
 
     public setContrasenaHash(contrasenaHash: string): void {
         this.contrasenaHash = contrasenaHash;
+    }
+
+    public setEmail(email: string): void {
+        this.email = email;
     }
 }
